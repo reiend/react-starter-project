88bc5acda1b0abb6c62e8d6be964a0a68950ff59
reiend <loncerasjakerei@gmail.com>
2022-07-20 09:04:02 +0800

    added jest and react testing lib for testing

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # react-starter-project
-A config template for starting new React project. Using webpack, esbuild, eslint, scss, postcss, autoprefixer, purgecss and tailwindscss.
+A config template for starting new React project. Using webpack, jest, esbuild, eslint, scss, postcss, autoprefixer, purgecss and tailwindscss.
---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  roots: ['<rootDir>'],
+  testEnvironment: 'jsdom',
+
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  // rootDir: './__test__/',
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less|scss)$': 'identity-obj-proxy',
+    '^@src(.*)$': '<rootDir>/src$1',
+    '^@components(.*)$': '<rootDir>/src/res/components$1',
+  },
+};
